9a4603f5ae1dd4d874407a3cdc497b8fa6b62330
Mohammed Khaled <mohammedimmortal10@gmail.com>
2022-05-17 20:22:04 +0200

    Record commit

--- lib/record.dart
+++ lib/record.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:path_provider/path_provider.dart';
 import 'package:record_mp3/record_mp3.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class Record extends StatefulWidget {
   const Record({Key? key}) : super(key: key);
@@ -15,56 +14,46 @@ class Record extends StatefulWidget {
 
 class _RecordState extends State<Record> {
   String statusText = "";
-  bool isComplete = false;
   bool isRecord = false;
+  RecordStatus _status = RecordStatus.IDEL;
+  RecordStatus get status => _status;
   String? recordFilePath = '';
   int i = 0;
 
-  Future<bool> checkPermission() async {
-    if (!await Permission.microphone.isGranted) {
-      PermissionStatus status = await Permission.microphone.request();
-      if (status != PermissionStatus.granted) {
-        return false;
-      }
-    }
-    return true;
-  }
-
   Future startRecord() async {
-    bool hasPermission = await checkPermission();
-    if (hasPermission) {
-      statusText = "Recording...";
-      recordFilePath = await getFilePath();
-      isComplete = false;
-      RecordMp3.instance.start(recordFilePath!, (type) {
-        statusText = "Record error--->$type";
-        setState(() {});
-      });
-    } else {
-      statusText = "No microphone permission";
-    }
-    setState(() {});
+    statusText = "Recording...";
+    _status = RecordStatus.RECORDING;
+    recordFilePath = await getFilePath();
+    RecordMp3.instance.start(recordFilePath!, (type) {
+      statusText = "Record error--->$type";
+      setState(() {});
+    });
   }
 
   Future stopRecord() async {
-    bool s = RecordMp3.instance.stop();
-    if (s) {
-      statusText = "Record complete";
-      isComplete = true;
+    if (_status == RecordStatus.RECORDING) {
+      statusText = "Record Saved in $recordFilePath";
+      RecordMp3.instance.stop();
+      _status = RecordStatus.IDEL;
       setState(() {});
     }
   }
 
+/*
   Future toggleRecord() async {
     if (isRecord == true) {
-      await startRecord();
-    } else {
       await stopRecord();
+    } else {
+      await startRecord();
     }
   }
+*/
 
-  Future play() async {
-    if (recordFilePath != null && File(recordFilePath!).existsSync()) {
+  Future playRecord() async {
+    if (recordFilePath != null &&
+        _status == RecordStatus.IDEL &&
+        File(recordFilePath!).existsSync()) {
+      statusText = 'Playing Record';
       AudioPlayer audioPlayer = AudioPlayer();
       audioPlayer.play(recordFilePath!, isLocal: true);
     }
@@ -73,9 +62,9 @@ class _RecordState extends State<Record> {
   Future<String> getFilePath() async {
     Directory storageDirectory = await getApplicationDocumentsDirectory();
     String sdPath = storageDirectory.path + "/record";
-    var d = Directory(sdPath);
-    if (!d.existsSync()) {
-      d.createSync(recursive: true);
+    final directory = Directory(sdPath);
+    if (!directory.existsSync()) {
+      directory.createSync(recursive: true);
     }
     return sdPath + "/test_${i++}.mp3";
   }
@@ -98,40 +87,35 @@ class _RecordState extends State<Record> {
                   decoration: BoxDecoration(color: Colors.red.shade300),
                   child: const Center(
                     child: Text(
-                      'start',
+                      'Start',
                       style: TextStyle(color: Colors.white),
                     ),
                   ),
                 ),
                 onTap: () async {
-                  setState(() {
-                    isRecord = true;
-                  });
-
-                  toggleRecord();
+                  setState(() {});
+                  startRecord();
                 },
               ),
             ),
-            /*
-              Expanded(
-                child: GestureDetector(
-                  child: Container(
-                    height: 48.0,
-                    decoration: BoxDecoration(color: Colors.blue.shade300),
-                    child: Center(
-                      child: Text(
-                        RecordMp3.instance.status == RecordStatus.PAUSE
-                            ? 'resume'
-                            : 'pause',
-                        style: const TextStyle(color: Colors.white),
-                      ),
+            Expanded(
+              child: GestureDetector(
+                child: Container(
+                  height: 48.0,
+                  decoration: BoxDecoration(color: Colors.blue.shade300),
+                  child: const Center(
+                    child: Text(
+                      'Play',
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
-                  onTap: () {
-                    pauseRecord();
-                  },
                 ),
-              ),*/
+                onTap: () async {
+                  setState(() {});
+                  playRecord();
+                },
+              ),
+            ),
             Expanded(
               child: GestureDetector(
                 child: Container(
@@ -139,71 +123,27 @@ class _RecordState extends State<Record> {
                   decoration: BoxDecoration(color: Colors.green.shade300),
                   child: const Center(
                     child: Text(
-                      'stop',
+                      'Stop',
                       style: TextStyle(color: Colors.white),
                     ),
                   ),
                 ),
                 onTap: () {
-                  setState(() {
-                    isRecord = false;
-                  });
-                  toggleRecord();
+                  setState(() {});
+                  stopRecord();
                 },
               ),
             ),
           ],
         ),
         Padding(
-          padding: const EdgeInsets.only(top: 20.0),
+          padding: const EdgeInsets.only(top: 40.0),
           child: Text(
             statusText,
             style: const TextStyle(color: Colors.red, fontSize: 20),
           ),
         ),
-        GestureDetector(
-          behavior: HitTestBehavior.opaque,
-          onTap: () {
-            play();
-          },
-          child: Container(
-            margin: const EdgeInsets.only(top: 30),
-            alignment: AlignmentDirectional.center,
-            width: 100,
-            height: 50,
-            child: isComplete && recordFilePath != null
-                ? const Text(
-                    "play",
-                    style: TextStyle(color: Colors.red, fontSize: 20),
-                  )
-                : Container(),
-          ),
-        ),
       ]),
     );
   }
-/*
-  Future pauseRecord() async {
-    if (RecordMp3.instance.status == RecordStatus.PAUSE) {
-      bool s = RecordMp3.instance.resume();
-      if (s) {
-        statusText = "Recording...";
-        setState(() {});
-      }
-    } else {
-      bool s = RecordMp3.instance.pause();
-      if (s) {
-        statusText = "Recording pause...";
-        setState(() {});
-      }
-    }
-  }
-
-  Future resumeRecord() async {
-    bool s = RecordMp3.instance.resume();
-    if (s) {
-      statusText = "Recording...";
-      setState(() {});
-    }
-  }*/
 }
